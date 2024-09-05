4ab35547bb12bc05dc49ad3586c3943f6c03bd28
IEarmando <aibarra7@ucol.com>
2024-09-05 15:44:57 -0600

    Initial commit

---
+++ app/src/main/java/com/example/kotlin_listado_de_lumnos/Alumno.kt
@@ -0,0 +1,7 @@
+package com.example.kotlin_listado_de_lumnos
+
+
+data class Alumno (val nombre: String, val numCuenta: String, val correo: String, val foto: String)
+
+
+
---
+++ app/src/main/java/com/example/kotlin_listado_de_lumnos/AlumnoAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.kotlin_listado_de_lumnos
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class AlumnoAdapter (private val context: Context, private val listAlumnos: List<Alumno>) : RecyclerView.Adapter<AlumnoAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlumnoAdapter.ViewHolder {
+
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_personas, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val ItemsViewModel = listAlumnos[position]
+
+        Glide.with(context).load(ItemsViewModel.foto).into(holder.imgFoto)
+
+        holder.nombre.text = ItemsViewModel.nombre
+        holder.numCuenta.text = ItemsViewModel.numCuenta
+    }
+
+    override fun getItemCount(): Int {
+        return listAlumnos.size
+    }
+
+    inner class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+        val imgFoto: ImageView = itemView.findViewById(R.id.img_personas)
+        val nombre: TextView = itemView.findViewById(R.id.tv_Nombre)
+        val numCuenta: TextView = itemView.findViewById(R.id.tv_Num_Cuenta)
+
+    }
+
+}
